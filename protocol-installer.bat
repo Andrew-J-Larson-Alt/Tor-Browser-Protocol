7d1393356062c80743cb938d735871d5931141d6
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2019-06-24 01:41:26 -0700

    Update protocol-installer.bat
    Fixed bugs

--- protocol-installer.bat
+++ protocol-installer.bat
@@ -2,7 +2,6 @@
 REM Created by AlienDrew thealiendrew@gmail.com
 REM PUT ME IN THE "Tor Browser" FOLDER
 
-setlocal
 :: BatchGotAdmin
 ::-------------------------------------
 REM  --> Check for permissions
@@ -20,7 +19,7 @@ if '%errorlevel%' NEQ '0' (
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
     "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
+    del /q "%temp%\getadmin.vbs"
     exit /B
 
 :gotAdmin
@@ -28,56 +27,52 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 ::--------------------------------------
 
-set "file_dir=%~dp0"
-cd "%file_dir%"
+if not exist "%cd%" goto error_install
+set "tor_dir=%cd%"
 
-if exist "%cd%\opentor.bat" (
-	set "type_action=uninstall"
-) else (
-	set "type_action=install"
-)
+set "type_action=install"
+if exist "%tor_dir%\opentor.bat" set "type_action=uninstall"
 
-if "%type_action%"=="install" (
-	if not exist "%cd%\Browser\firefox.exe" goto error_install
-)
-
-choice /n /c YE /m "Do you wish to %type_action% the Tor Browser? ([Y]es, [E]xit)"
+choice /n /c YE /m "Do you wish to %type_action% the Tor Browser protocol? ([Y]es, [E]xit)"
 if errorlevel 2 goto end_it
 echo.
+if "%type_action%"=="uninstall" goto uninstall
 
 :install
 REM create protocol script
-if exist "%cd%\opentor.bat" del /f "%cd%\opentor.bat"
-echo @echo off > "%cd%\opentor.bat"
-echo if [%%1] == [] goto :eof >> "%cd%\opentor.bat"
-echo set "browser_dir=%%userprofile%%\Desktop\Tor Browser\Browser\firefox.exe" >> "%cd%\opentor.bat"
-echo set "url=%%1" >> "%cd%\opentor.bat"
-echo echo %%1 ^| find "tor-browser://" ^> nul >> "%cd%\opentor.bat"
-echo if errorlevel 0 set "url=%%url:~15,-1%%" >> "%cd%\opentor.bat"
-echo tasklist /fi "imagename eq firefox.exe" ^| find ":" ^> nul >> "%cd%\opentor.bat"
-echo if errorlevel 1 ( >> "%cd%\opentor.bat"
-echo start /b "" "%%browser_dir%%" --allow-remote --new-tab "%%url%%" >> "%cd%\opentor.bat"
-echo exit >> "%cd%\opentor.bat"
-echo ) >> "%cd%\opentor.bat"
-echo start /b "" "%%browser_dir%%" --allow-remote "%%url%%" >> "%cd%\opentor.bat"
-echo exit >> "%cd%\opentor.bat"
+if not exist "%tor_dir%\Browser\firefox.exe" goto error_install
+if exist "%tor_dir%\opentor.bat" del /f /q "%tor_dir%\opentor.bat"
+echo @echo off > "%tor_dir%\opentor.bat"
+echo if [%%1] == [] goto :eof >> "%tor_dir%\opentor.bat"
+echo set "browser_dir=%tor_dir%\Browser\firefox.exe" >> "%tor_dir%\opentor.bat"
+echo set "url=%%1" >> "%tor_dir%\opentor.bat"
+echo echo %%1 ^| find "tor-browser://" ^> nul >> "%tor_dir%\opentor.bat"
+echo if errorlevel 0 set "url=%%url:~15,-1%%" >> "%tor_dir%\opentor.bat"
+echo tasklist /fi "imagename eq firefox.exe" ^| find ":" ^> nul >> "%tor_dir%\opentor.bat"
+echo if errorlevel 1 ( >> "%tor_dir%\opentor.bat"
+echo start /b "" "%%browser_dir%%" --allow-remote --new-tab "%%url%%" >> "%tor_dir%\opentor.bat"
+echo exit >> "%tor_dir%\opentor.bat"
+echo ) >> "%tor_dir%\opentor.bat"
+echo start /b "" "%%browser_dir%%" --allow-remote "%%url%%" >> "%tor_dir%\opentor.bat"
+echo exit >> "%tor_dir%\opentor.bat"
 REM registry install
-reg add HKCR\tor-browser /ve /d "URL:tor-browser Protocol" /f
-reg add HKCR\tor-browser /v "URL Protocol" /t REG_SZ /f
-reg add HKCR\tor-browser\shell\open\command /ve /d "\"%tor_dir%\opentor.bat\" \"%%1\"" /f
-::--------------------
-echo Installation successful.
+reg add HKCR\tor-browser /ve /d "URL:tor-browser Protocol" /f > nul
+reg add HKCR\tor-browser /v "URL Protocol" /t REG_SZ /f > nul
+reg add HKCR\tor-browser\shell\open\command /ve /d "\"%tor_dir%\opentor.bat\" \"%%1\"" /f > nul
+echo Successfully installed.
 goto end_it
 
 :uninstall
-reg delete HKCR\tor-browser /f
-if exist "%cd%\opentor.bat" del /f "%cd%\opentor.bat"
+reg delete HKCR\tor-browser /f > nul
+if exist "%tor_dir%\opentor.bat" del /f /q "%tor_dir%\opentor.bat"
+echo Successfully uninstalled.
+goto end_it
 
 :error_install
 echo Error: Missing the files needed to install...
 echo Please make sure this script is inside the "Tor Browser" folder
+echo Unsuccessfully installed.
 
 :end_it
 echo. && pause
-endlocal
 exit
