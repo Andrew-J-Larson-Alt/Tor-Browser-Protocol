621503ea3279065fd55be0ebaaeb869dcd51df5c
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-05-24 18:25:01 -0700

    The licensing

--- protocol-installer.bat
+++ protocol-installer.bat
@@ -1,5 +1,20 @@
 @echo off
-REM Created by AlienDrew thealiendrew@gmail.com
+REM This protocol allows for redirecting tor browser links to the TOR browser from inside other browsers/web based programs.
+REM Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
+REM
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 REM PUT ME IN THE "Tor Browser" FOLDER
 
 :: BatchGotAdmin
